d32374abf3c763b9eff3e3683a45ab4ffb505337
Syuzanna Manukyan <syuzmanukyan0201@gmail.com>
2022-04-01 14:18:49 +0400

    .

--- File/Program.cs
+++ File/Program.cs
@@ -6,6 +6,15 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+
+            Write();
+            WriteTry();
+            Readtxt();
+
+
+        }
+        static void Write()
+        {
             string file = @"c:\input\text.txt";
             string file1 = @"c:\input\text1.txt";
             string l = "Hello Word";
@@ -13,7 +22,10 @@ namespace ConsoleApp1
 
             File.Copy(file, file1);
             File.Delete(file);
-             
+
+        }
+        static void WriteTry()
+        {
             string filetry = @"c:\input\texttry.txt";
             int x = 0;
             try
@@ -25,6 +37,17 @@ namespace ConsoleApp1
                 string L = "Exclusion error:";
                 File.WriteAllText(filetry, L);
             }
+
+        }
+        static void Readtxt()
+        {
+            string file1 = @"c:\input\text1.txt";
+            string items = File.ReadAllText(file1);
+            foreach (var item in items)
+            {
+                Console.Write(item);
+            }
+            File.Delete(file1);
             Console.ReadKey();
         }
     }
