727bc973f9a9246ac4c4686de615fe7039842a63
Syuzanna Manukyan <syuzmanukyan0201@gmail.com>
2022-04-01 14:40:23 +0400

    .

---
+++ File2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace File2
+{
+    class Program
+    {
+        static void Main()
+        {
+            FileStream opend = new FileStream(@"c:\input\textob.txt", FileMode.OpenOrCreate);
+
+            Console.WriteLine("File opened");
+
+            StreamWriter whrite = new StreamWriter(opend);
+
+            whrite.WriteLine("Hi!");
+
+            whrite.Close();
+        }
+    }
+}
