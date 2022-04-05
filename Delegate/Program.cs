f77cf014f1ccad53d4ebdfc2e408d98d96450cef
Syuzanna Manukyan <syuzmanukyan0201@gmail.com>
2022-04-05 16:48:58 +0400

    .

---
+++ Delegate/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Delegate
+{
+    class Program
+    {
+        public delegate int Delegates(ref int a);
+        static void Main()
+        {
+            int a = 1;
+            Delegates delegates = Method1;
+            delegates += Method2;
+            delegates += Method3;
+            delegates += Method4;
+            Console.WriteLine(delegates(ref a));
+        }
+        static int Method1(ref int a)
+        {
+            return a*=2;
+        }
+        static int Method2(ref int a)
+        {
+            int p = 5;
+            return a*=p;
+        }
+        static int Method3(ref int a)
+        {
+            int r = 6;
+            return a*=r;
+        }
+        static int Method4(ref int a) 
+        {
+            return a*=a;
+        }
+    }
+}
---
+++ RandomAndSleep/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace RandomAndSleep
+{
+    class Program
+    {
+        static void Main()
+        {
+            Random random = new Random();
+
+            int n = int.Parse(Console.ReadLine());
+
+            int m = int.Parse(Console.ReadLine());
+
+            int[,] arr = new int[n, m];
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    arr[i, j] = random.Next(100);
+                }
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(arr[i, j] + ",  ");
+
+                    Thread.Sleep(800);
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
